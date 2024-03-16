287e4610c14ecda85b1e7f8c8758a66e3e732d07
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-16 16:32:43 +0000

    Added stuff idk

--- formation/src/app/app.routes.ts
+++ formation/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListeUtilisateursComponent } from './components/utilisateurs/liste-utilisateurs/liste-utilisateurs.component';
+// import { RouterModule , Router}
 
-export const routes: Routes = [];
+export const routes: Routes = [ {
+    path: '',
+    component: ListeUtilisateursComponent
+}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
---
+++ formation/src/app/components/utilisateurs/liste-utilisateurs/liste-utilisateurs.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Utilisateur } from '../../../models/Utilisateurs.model';
+
+@Component({
+  selector: 'app-liste-utilisateurs',
+  standalone: true,
+  imports: [],
+  templateUrl: './liste-utilisateurs.component.html',
+  styleUrl: './liste-utilisateurs.component.scss'
+})
+
+export class ListeUtilisateursComponent {
+utilisateurs: Utilisateur[]=[
+  {
+  id:'001',
+  nom:'utilisateur1',
+  prenom:'ww',
+  phone:123456789
+  },
+  {
+    id:'002',
+    nom:'utilisateur2',
+    prenom:'ww',
+    phone:456789123
+    },
+];
+constructor() {}
+ngOnInit(): void {
+  
+}
+}
